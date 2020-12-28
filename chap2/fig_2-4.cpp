ed1852f37c793fdad23474049d75822cdf737e6f
Jonathan Hardison <jmh@jonathanhardison.com>
2020-12-28 16:32:03 -0600

    fig_2-5 and changes to gitignore.

---
+++ chap2/fig_2-4.cpp
@@ -0,0 +1,10 @@
+//
+// Created by jonathan on 12/28/20.
+//
+
+#include <iostream>
+
+int main() {
+
+    std::cout << "Welcome \n to \n C++!";
+}
---
+++ chap2/fig_2-5.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main(){
+
+    int number1{0};
+    int number2{0};
+    int sum{0};
+
+    std::cout << "Enter first number: ";
+    std::cin >> number1;
+    std::cout << "Enter second number: ";
+    std::cin >> number2;
+
+    sum = number1 + number2;
+
+    std::cout << "The sum is: " << sum;
+
+}
