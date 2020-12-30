108de72be76752c33424e963909b174c206d6c4d
Jonathan Hardison <jmh@jonathanhardison.com>
2020-12-29 21:22:55 -0600

    eol 2.21

---
+++ chap2/eol_2-21.cpp
@@ -0,0 +1,21 @@
+/*
+write a program that prints C++ as follows
+  CCC    +      +
+ C       +      +
+C      +++++  +++++
+ C       +      +
+  CCC    +      +
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "  CCC    +      +  " << endl;
+    cout << " C       +      +  " << endl;
+    cout << "C      +++++  +++++" << endl;
+    cout << " C       +      +  " << endl;
+    cout << "  CCC    +      +  " << endl;
+
+}
