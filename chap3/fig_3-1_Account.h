846bfb00970833a45831424a3c44f8f64ca63316
Jonathan Hardison <jmh@jonathanhardison.com>
2020-12-31 23:18:56 -0600

    git change and fig3-1

---
+++ chap3/fig_3-1_Account.h
@@ -0,0 +1,16 @@
+#include <string>
+
+
+class Account{
+    public:
+        void setName(std::string accountName){
+            name = accountName;
+        }
+
+        std::string getName() const {
+            return name;
+        }
+
+    private:
+        std::string name;
+};
